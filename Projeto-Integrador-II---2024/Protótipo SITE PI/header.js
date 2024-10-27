db7df22510eb25333070c6f4284b244d69a3a797
João Guilherme Vargas Pasinatto <134635474+JoaoGVP001@users.noreply.github.com>
2024-10-27 09:15:20 -0300

    Create header.js

---
+++ Projeto-Integrador-II---2024/Protótipo SITE PI/header.js
@@ -0,0 +1,6 @@
+function toggleDropdown() {
+    const dropdown = document.getElementById('dropdown');
+    const overlay = document.getElementById('overlay');
+    dropdown.classList.toggle('hidden');
+    overlay.classList.toggle('hidden');
+}
