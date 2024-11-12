1d6c2148a67ea9b06fb53b49046b10e4822c8f96
Astoran07 <140649361+Astoran07@users.noreply.github.com>
2024-11-12 14:26:37 -0300

    Add files via upload

--- WebSite/Js/effects.js
+++ WebSite/Js/effects.js
@@ -4,3 +4,11 @@ function toggleDropdown() {
     dropdown.classList.toggle('hidden');
     overlay.classList.toggle('hidden');
 }
+
+document.getElementById('back-to-top').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
